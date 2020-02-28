36f40eeb15de54334d759f4b4ce29ec00e3de0f4
Ryan Zhu <ryanxzhu@gmail.com>
2020-02-27 20:27:43 -0500

    removed references to mongoose as this app doesn't require a database

--- app.js
+++ app.js
@@ -3,17 +3,9 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const express = require('express'),
-	app = express(),
-	mongoose = require('mongoose');
+	app = express();
 
 const indexRouter = require('./routes/index');
-
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-
-const db = mongoose.connection;
-
-db.on('error', (err) => console.error(err));
-db.once('open', () => console.log('Connected to Mongoose'));
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
